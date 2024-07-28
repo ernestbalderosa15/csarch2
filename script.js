24282d350b2dae013685822877136f6e1b0b528d
Ernest Y. Balderosa <sofia_balderosa@dlsu.edu.ph>
2024-07-28 13:23:31 +0800

    Initial Commit
    Worked on majority of the features. Total Access Time not yet implemented. Unsure if implementation is correct.

---
+++ script.js
@@ -0,0 +1,69 @@
+document.getElementById('cacheForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let blockSize = parseInt(document.getElementById('blockSize').value);
+    let MMSize = parseInt(document.getElementById('MMSize').value);
+    let cacheSize = parseInt(document.getElementById('cacheSize').value);
+    let cacheAccessTime = parseInt(document.getElementById('cacheAccessTime').value);
+    let memoryAccessTime = parseInt(document.getElementById('memoryAccessTime').value);
+    let programFlow = document.getElementById('programFlow').value.split(',').map(Number);
+
+    let missPenalty = cacheAccessTime * 2 + memoryAccessTime * blockSize;
+
+    let cacheHit = 0;
+    let cacheMiss = 0;
+    let currIdx = 0;
+    let blocks = new Array(cacheSize);
+    let cacheHistory = [];
+
+    programFlow.forEach(elem => {
+        if (blocks.includes(elem)) {
+            cacheHit += 1;
+            currIdx = blocks.indexOf(elem);
+        } else if (blocks[currIdx] === null || blocks[currIdx] === undefined) {
+            blocks[currIdx] = elem;
+            cacheMiss += 1;
+            if (currIdx < cacheSize - 1) {
+                currIdx++;
+            }
+        } else {
+            cacheMiss += 1;
+            blocks[currIdx] = elem;
+        }
+        cacheHistory.push([...blocks]);
+    });
+
+    function calcAveAT() {
+        let hitRate = cacheHit / programFlow.length;
+        let averageAccess = hitRate * cacheAccessTime + (1 - hitRate) * missPenalty;
+        return averageAccess.toFixed(2);
+    }
+
+    let averageAccessTime = calcAveAT();
+
+    //TO DO
+    function totalAccessTime(){
+
+    }
+
+    let output = `
+        <p>Number of cache hits: ${cacheHit}/${programFlow.length}</p>
+        <p>Number of cache misses: ${cacheMiss}/${programFlow.length}</p>
+        <p>Miss penalty: ${missPenalty}ns</p>
+        <p>Average memory access time: ${averageAccessTime}ns</p>
+        <h3>Cache Memory History:</h3>
+        <pre>${JSON.stringify(cacheHistory, null, 2)}</pre>
+    `;
+
+    document.getElementById('output').innerHTML = output;
+    document.getElementById('downloadBtn').style.display = 'block';
+
+    document.getElementById('downloadBtn').onclick = function() {
+        let historyText = cacheHistory.map((step, index) => `Iteration ${index + 1}: ${step.join(', ')}`).join('\n');
+        let blob = new Blob([historyText], { type: 'text/plain' });
+        let link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'cache_history.txt';
+        link.click();
+    };
+});
